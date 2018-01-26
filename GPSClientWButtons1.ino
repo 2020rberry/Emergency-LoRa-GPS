e40111c9ce1a914ffab8247da954bc41c01b5eac
2020rberry <2020rberry@tjhsst.edu>
2018-01-26 18:05:14 -0500

    Add files via upload

---
+++ GPSClientWButtons1.ino
@@ -0,0 +1,115 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define RFM95_CS 10
+#define RFM95_RST 9
+#define RFM95_INT 2
+
+RH_RF95 rf95;
+TinyGPS gps;
+float ffinaladdition;
+char cfinal [1];
+float fnode;
+char cnode [1];
+uint8_t dataoutgoing[50];
+
+void setup()
+{
+    pinMode(3, INPUT);
+    pinMode(4, INPUT);
+    pinMode(5, INPUT);
+    Serial.begin(9600);
+    if (!rf95.init())
+      Serial.println("init failed");
+    RH_RF95::ModemConfig modem_config = {
+      0x78, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit
+      0xc4, // Reg 0x1E: Spread=4096chips/symbol, CRC=enable
+      0x0c  // Reg 0x26: LowDataRate=On, Agc=On
+    };
+    rf95.setModemRegisters(&modem_config);
+    //Serial.print("Simple TinyGPS library v. "); Serial.println(TinyGPS::library_version());
+    //Serial.println();
+}
+
+void loop()
+{ 
+  while(digitalRead(3) == LOW && digitalRead(4) == LOW && digitalRead(5) == LOW)
+  {}
+  if(digitalRead(3) == HIGH)
+  {
+    ffinaladdition = 1;
+  }
+  else if(digitalRead(4) == HIGH)
+  {
+    ffinaladdition = 2;
+  }
+  else
+  {
+    ffinaladdition = 3;
+  }
+  Serial.println("Sending to rf95_server");
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (Serial.available())
+    {
+      char c = Serial.read();
+      if (gps.encode(c)) 
+      newData = true;
+    }
+  }
+  if (newData)
+  {
+    float flat, flon;
+    fnode = 1;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    Serial.print("LAT=");
+    Serial.print(flat, 4);
+    Serial.print(" LON=");
+    Serial.println(flon, 4);
+
+    char clat [10];
+    char clon [10];
+    dtostrf(flat, 9, 4, clat);
+    dtostrf(flon, 9, 4, clon); 
+    dtostrf(ffinaladdition, 1, 0, cfinal);
+
+    strcat(clat, ", ");
+    strcat(clat, clon);
+    strcat(clat, " : ");
+    strcat(clat, cfinal);
+    dtostrf(fnode, 1, 0, cnode);
+    strcat(clat, cnode);
+    Serial.println(clat);
+    strcpy((char *)dataoutgoing,clat); 
+    rf95.send(dataoutgoing, sizeof(dataoutgoing));
+   
+    uint8_t indatabuf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(indatabuf);
+
+    if (rf95.waitAvailableTimeout(3000))
+     { 
+       if (rf95.recv(indatabuf, &len))
+      {
+         Serial.print("got reply: ");
+         Serial.println((char*)indatabuf);
+      }
+      else
+      {
+      Serial.println("recv failed");
+      }
+    }
+    else
+    {
+      Serial.println("No reply, is rf95_server running?");
+    }
+  delay(400);
+  
+ }
+}
---
+++ GPSServer.ino
@@ -0,0 +1,109 @@
+
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <EEPROM.h>
+RH_RF95 rf95;
+int led = 13;
+int reset_lora = 9;
+String dataString = "";
+int address = 0;
+char eepromCheck[50];
+#define RFM95_CS 10
+#define RFM95_RST 9
+#define RFM95_INT 2
+
+void setup() 
+{
+  Serial.begin(9600);
+  if (!rf95.init())
+    Serial.println("init failed");
+  RH_RF95::ModemConfig modem_config = {
+      0x78, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit
+      0xc4, // Reg 0x1E: Spread=4096chips/symbol, CRC=enable
+      0x0c  // Reg 0x26: LowDataRate=On, Agc=On
+    };
+  rf95.setModemRegisters(&modem_config);
+  for(int i = 0; i < EEPROM.length(); i++)
+  {
+    EEPROM.write(i, -1);
+  }
+  Serial.println("Ready");
+}
+
+void loop()
+{
+  int count = 0;
+  dataString="";
+  if (rf95.available())
+  {
+    Serial.println("Get new message");
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf95.recv(buf, &len))
+    {
+      dataString += String((char*)buf);
+      uint8_t data[] = "Coordinates Recieved";
+      rf95.send(data, sizeof(data));
+      rf95.waitPacketSent();
+      Serial.print("Coordinates: ");
+      String coordinates = getCoordinates(dataString);
+      String severity = getSeverity(dataString);
+      String severityMessage = "";
+      if(severity.equals("1"))
+      {
+        severityMessage = "Low on Supplies";
+      }
+      else if(severity.equals("2"))
+      {
+        severityMessage = "Need Medical Assistance";
+      }
+      else
+      {
+        severityMessage = "Everything ok";
+      }
+      Serial.println(coordinates);
+      Serial.print("Status: ");
+      Serial.println(severityMessage);
+      Serial.print("Node: ");
+      Serial.println(getNode(dataString));
+      if(address < EEPROM.length())
+      {
+        Serial.println(len);
+        for(int k = 0; k < len; k++)
+        {
+          EEPROM[address + k] = buf[k];
+        }
+        Serial.println("end");
+        address = address + len;
+      }
+      else
+      {
+        Serial.println("EEPROM full");
+      }
+      Serial.println("");
+    }
+    else
+    {
+      Serial.println("recv failed");
+    }
+  }
+  
+}
+String getSeverity(String data)
+{
+  String temp = data.substring(data.indexOf(":") + 2, data.indexOf(":") + 3);
+  temp.trim();
+  return temp;
+}
+String getNode(String data)
+{
+  String temp = data.substring(data.indexOf(":") + 3);
+  temp.trim();
+  return temp;
+}
+String getCoordinates(String data)
+{
+  String temp = data.substring(0, data.indexOf(":"));
+  temp.trim();
+  return temp;
+}
